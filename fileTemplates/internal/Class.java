3ec7e89a036a455bd444e47d4f0174c663abe800
yuanjiajun <768747811@qq.com>
2021-01-07 09:48:30 +0800

    IU-2020.2.3 <yuanjiajun@yuanLab Create baseRefactoring.xml, markdown.xml, project.default.xml, diff.xml, shared-indexes.xml, Default.xml, Default for Windows copy Proper Redo.xml, find.xml, ui.lnf.xml, filetypes.xml, keymap.xml, colors.scheme.xml, databaseDrivers.xml, github.xml, mavenVersion.xml, templates.xml, Default.xml, Default for Windows copy.xml, laf.xml, Class.java, jdk.table.xml, MyFunction.xml, path.macros.xml, lightEdit.xml, intentionSettings.xml, wsl.distributions.xml, vcs.xml, grazie_global.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+* @author yuanjiajun
+* @version
+* @description
+* @date ${DATE} ${TIME}
+*
+*/
+public class ${NAME} {
+}
