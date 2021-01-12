ae521926fcb77b4194311d9241081b7e4ebcaacb
yuanjiajun <767847811@qq.com>
2021-01-12 21:29:50 +0800

    IU-2020.2.3 <yuanjiajun@yuanPC Create markdown.xml, Windows copy.xml, project.default.xml, rainbow_brackets.xml, laf.xml, Class.java, jdk.table.xml, shared-indexes.xml, letGoGroop.xml, Default.xml, path.macros.xml, lightEdit.xml, ignore.xml, wsl.distributions.xml, filetypes.xml, vcs.xml, colors.scheme.xml, grazie_global.xml, mybatis.xml, databaseDrivers.xml, github.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,12 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+* @author yuanjiajun
+* @version
+* @description
+* @date ${DATE} ${TIME}
+*
+*/
+public class ${NAME} {
+}
